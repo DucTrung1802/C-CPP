9dbfa0ef99aca76afd4a26092d554eac6fbeea5c
Duc Trung <trung.lyduc18@gmail.com>
2023-09-06 23:06:19 +0700

    CHANGE: host_000_test_project

--- Embedded/Workspace/host_000_test_project/main.c
+++ Embedded/Workspace/host_000_test_project/main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
+void myFunc1();
+void myFunc2();
+
 int main(void) {
-	printf("Hello world\n");
+	myFunc1();
+	myFunc1();
+	myFunc1();
+	myFunc1();
 
-	printf("Press 'Enter' to exit this application\n");
-
-	getchar();
+	myFunc2();
+	myFunc2();
+	myFunc2();
+	myFunc2();
 
 	return 0;
+}
+
+void myFunc1() {
+	static int count = 0;
+	count = count + 1;
+	printf("Count func1 = %d\n", count);
+}
+
+void myFunc2() {
+	static int count = 100;
+	count = count + 1;
+	printf("Count func2 = %d\n", count);
 }
