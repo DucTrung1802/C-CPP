a184ac8a627dc4db696ee637e414dfce83302ca7
Duc Trung <trung.lyduc18@gmail.com>
2023-09-22 18:08:57 +0700

    COMPLETE: target_007_keypad

--- Embedded/Workspace/target_007_keypad/Src/main.c
+++ Embedded/Workspace/target_007_keypad/Src/main.c
@@ -16,8 +16,11 @@
  ******************************************************************************
  */
 
+#include <stdio.h>
 #include <stdint.h>
 #include "main.h"
+
+#define DELAY_AMOUNT 300000
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -36,35 +39,46 @@
  *
  */
 
+void delayms(uint32_t const time_value);
+
 int main(void)
 {
 	/* Reset and clock control reg */
 	RCC_AHB1_ENR_t
-	volatile *const p_clock_control_reg = (RCC_AHB1_ENR_t*) RCC;
+	volatile *const p_clock_control_reg = (RCC_AHB1_ENR_t*) (RCC + 0x30);
 
 	/* GPIO A Configuration */
 	p_clock_control_reg->gpio_a_en = 1;
 	GPIOx_MODE_REG_t volatile *const p_port_a_mode_reg =
 			(GPIOx_MODE_REG_t*) GPIO_A;
-	GPIOx_PUPDR_t volatile *const p_port_a_pull_reg = (GPIOx_PUPDR_t*) GPIO_A;
-	GPIOx_IDR_t volatile const *const p_port_a_input_reg = (GPIOx_IDR_t*) GPIO_A;
-	GPIOx_ODR_t volatile *const p_port_a_output_reg = (GPIOx_ODR_t*) GPIO_A;
+	GPIOx_PUPDR_t volatile *const p_port_a_pull_reg = (GPIOx_PUPDR_t*) (GPIO_A
+			+ 0x0C);
+	GPIOx_IDR_t volatile const *const p_port_a_input_reg =
+			(GPIOx_IDR_t*) (GPIO_A + 0x10);
+	GPIOx_ODR_t volatile *const p_port_a_output_reg = (GPIOx_ODR_t*) (GPIO_A
+			+ 0x14);
 
 	/* GPIO B Configuration */
 	p_clock_control_reg->gpio_b_en = 1;
 	GPIOx_MODE_REG_t volatile *const p_port_b_mode_reg =
 			(GPIOx_MODE_REG_t*) GPIO_B;
-	GPIOx_PUPDR_t volatile *const p_port_b_pull_reg = (GPIOx_PUPDR_t*) GPIO_B;
-	GPIOx_IDR_t volatile const *const p_port_b_input_reg = (GPIOx_IDR_t*) GPIO_B;
-	GPIOx_ODR_t volatile *const p_port_b_output_reg = (GPIOx_ODR_t*) GPIO_B;
+	GPIOx_PUPDR_t volatile *const p_port_b_pull_reg = (GPIOx_PUPDR_t*) (GPIO_B
+			+ 0x0C);
+	GPIOx_IDR_t volatile const *const p_port_b_input_reg =
+			(GPIOx_IDR_t*) (GPIO_B + 0x10);
+	GPIOx_ODR_t volatile *const p_port_b_output_reg = (GPIOx_ODR_t*) (GPIO_B
+			+ 0x14);
 
 	/* GPIO C Configuration */
 	p_clock_control_reg->gpio_c_en = 1;
 	GPIOx_MODE_REG_t volatile *const p_port_c_mode_reg =
 			(GPIOx_MODE_REG_t*) GPIO_C;
-	GPIOx_PUPDR_t volatile *const p_port_c_pull_reg = (GPIOx_PUPDR_t*) GPIO_C;
-	GPIOx_IDR_t volatile const *const p_port_c_input_reg = (GPIOx_IDR_t*) GPIO_C;
-	GPIOx_ODR_t volatile *const p_port_c_output_reg = (GPIOx_ODR_t*) GPIO_C;
+	GPIOx_PUPDR_t volatile *const p_port_c_pull_reg = (GPIOx_PUPDR_t*) (GPIO_C
+			+ 0x0C);
+	GPIOx_IDR_t volatile const *const p_port_c_input_reg =
+			(GPIOx_IDR_t*) (GPIO_C + 0x10);
+	GPIOx_ODR_t volatile *const p_port_c_output_reg = (GPIOx_ODR_t*) (GPIO_C
+			+ 0x14);
 
 	/* Input pins */
 	/* PC1 - C4 (input) */
@@ -96,18 +110,206 @@ int main(void)
 	/* PB1 - R1 (output) */
 	p_port_b_mode_reg->pin_1 = OUTPUT_MODE;
 
-	uint8_t row_output_address[4] =
-	{ p_port_b_output_reg->pin_1, p_port_c_output_reg->pin_5,
-			p_port_a_output_reg->pin_7, p_port_a_output_reg->pin_5 };
-
-	uint8_t col_input_address[4] =
-	{ p_port_a_input_reg->pin_3, p_port_a_input_reg->pin_1,
-			p_port_c_input_reg->pin_3, p_port_c_input_reg->pin_1 };
-
 	printf("Hello world!\n");
+
+	/* Set output high */
+	p_port_b_output_reg->pin_1 = 1;
+	p_port_c_output_reg->pin_5 = 1;
+	p_port_a_output_reg->pin_7 = 1;
+	p_port_a_output_reg->pin_5 = 1;
+
 	/* Loop forever */
 	while (1)
 	{
+		for (int i = 0; i < 4; i++)
+		{
+			switch (i)
+			{
+			case 0:
+				p_port_b_output_reg->pin_1 = 0;
 
+				for (int j = 0; j < 4; j++)
+				{
+					switch (j)
+					{
+					case 0:
+						if (!(p_port_a_input_reg->pin_3))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("1\n");
+						}
+						break;
+
+					case 1:
+						if (!(p_port_a_input_reg->pin_1))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("2\n");
+						}
+						break;
+
+					case 2:
+						if (!(p_port_c_input_reg->pin_3))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("3\n");
+						}
+						break;
+
+					case 3:
+						if (!(p_port_c_input_reg->pin_1))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("A\n");
+						}
+						break;
+					}
+				}
+
+				p_port_b_output_reg->pin_1 = 1;
+				break;
+
+			case 1:
+				p_port_c_output_reg->pin_5 = 0;
+
+				for (int j = 0; j < 4; j++)
+				{
+					switch (j)
+					{
+					case 0:
+						if (!(p_port_a_input_reg->pin_3))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("4\n");
+						}
+						break;
+
+					case 1:
+						if (!(p_port_a_input_reg->pin_1))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("5\n");
+						}
+						break;
+
+					case 2:
+						if (!(p_port_c_input_reg->pin_3))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("6\n");
+						}
+						break;
+
+					case 3:
+						if (!(p_port_c_input_reg->pin_1))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("B\n");
+						}
+						break;
+					}
+				}
+
+				p_port_c_output_reg->pin_5 = 1;
+				break;
+
+			case 2:
+				p_port_a_output_reg->pin_7 = 0;
+
+				for (int j = 0; j < 4; j++)
+				{
+					switch (j)
+					{
+					case 0:
+						if (!(p_port_a_input_reg->pin_3))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("7\n");
+						}
+						break;
+
+					case 1:
+						if (!(p_port_a_input_reg->pin_1))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("8\n");
+						}
+						break;
+
+					case 2:
+						if (!(p_port_c_input_reg->pin_3))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("9\n");
+						}
+						break;
+
+					case 3:
+						if (!(p_port_c_input_reg->pin_1))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("C\n");
+						}
+						break;
+					}
+				}
+
+				p_port_a_output_reg->pin_7 = 1;
+				break;
+
+			case 3:
+				p_port_a_output_reg->pin_5 = 0;
+
+				for (int j = 0; j < 4; j++)
+				{
+					switch (j)
+					{
+					case 0:
+						if (!(p_port_a_input_reg->pin_3))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("*\n");
+						}
+						break;
+
+					case 1:
+						if (!(p_port_a_input_reg->pin_1))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("0\n");
+						}
+						break;
+
+					case 2:
+						if (!(p_port_c_input_reg->pin_3))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("#\n");
+						}
+						break;
+
+					case 3:
+						if (!(p_port_c_input_reg->pin_1))
+						{
+							delayms(DELAY_AMOUNT);
+							printf("D\n");
+						}
+						break;
+					}
+				}
+
+				p_port_a_output_reg->pin_5 = 1;
+				break;
+			}
+		}
+
+	}
+}
+
+void delayms(uint32_t const time_value)
+{
+	for (int i = time_value; i > 0; i--)
+	{
+		// NOP
 	}
 }
