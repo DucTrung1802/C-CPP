34b0f74cf411a63a9b5a8262e182cb8fb5252ca4
Duc Trung <trung.lyduc18@gmail.com>
2023-10-03 22:42:06 +0700

    FIX: target_016_bare_metal_embedded

--- Embedded/Workspace/target_016_bare_metal_embedded/stm32_startup.c
+++ Embedded/Workspace/target_016_bare_metal_embedded/stm32_startup.c
@@ -1,10 +1,21 @@
 #include <stdint.h>
 
-#define SRAM_START 0x20000000U
-#define SRAM_SIZE (128U * 1024U) // 128KB
-#define SRAM_END ((SRAM_START) + (SRAM_SIZE))
+#define SRAM_START      0x20000000U
+#define SRAM_SIZE       (128U * 1024U) // 128KB
+#define SRAM_END        ((SRAM_START) + (SRAM_SIZE))
 
-#define STACK_START SRAM_END
+#define STACK_START     SRAM_END
+
+extern uint32_t end_of_text;
+extern uint32_t start_of_data;
+extern uint32_t end_of_data;
+extern uint32_t start_of_bss;
+extern uint32_t end_of_bss;
+
+/* Function prototype of main */
+int main(void);
+
+/* Function prototypes of STM32F411 system exception and IRQ handlers */
 
 void Reset_Handler(void);
 
@@ -79,85 +90,108 @@ uint32_t vectors[] __attribute__((section(".isr_vector"))) =
     {
         STACK_START, // MSP
         (uint32_t)&Reset_Handler,
-        (uint32_t)NMI_Handler,
-        (uint32_t)HardFault_Handler,
-        (uint32_t)MemManage_Handler,
-        (uint32_t)BusFault_Handler,
-        (uint32_t)UsageFault_Handler,
+        (uint32_t)&NMI_Handler,
+        (uint32_t)&HardFault_Handler,
+        (uint32_t)&MemManage_Handler,
+        (uint32_t)&BusFault_Handler,
+        (uint32_t)&UsageFault_Handler,
         0,
         0,
         0,
         0,
-        (uint32_t)SVC_Handler,
-        (uint32_t)DebugMon_Handler,
+        (uint32_t)&SVC_Handler,
+        (uint32_t)&DebugMon_Handler,
         0,
-        (uint32_t)PendSV_Handler,
-        (uint32_t)Systick_Handler,
-        (uint32_t)WWDG_IRQHandler,
-        (uint32_t)PVD_IRQHandler,
-        (uint32_t)TAMP_STAMP_IRQHandler,
-        (uint32_t)RTC_WKUP_IRQHandler,
-        (uint32_t)FLASH_IRQHandler,
-        (uint32_t)RCC_IRQHandler,
-        (uint32_t)EXTI0_IRQHandler,
-        (uint32_t)EXTI1_IRQHandler,
-        (uint32_t)EXTI2_IRQHandler,
-        (uint32_t)EXTI3_IRQHandler,
-        (uint32_t)EXTI4_IRQHandler,
-        (uint32_t)DMA1_Stream0_IRQHandler,
-        (uint32_t)DMA1_Stream1_IRQHandler,
-        (uint32_t)DMA1_Stream2_IRQHandler,
-        (uint32_t)DMA1_Stream3_IRQHandler,
-        (uint32_t)DMA1_Stream4_IRQHandler,
-        (uint32_t)DMA1_Stream5_IRQHandler,
-        (uint32_t)DMA1_Stream6_IRQHandler,
-        (uint32_t)ADC_IRQHandler,
-        (uint32_t)EXTI9_5_IRQHandler,
-        (uint32_t)TIM1_BRK_TIM9_IRQHandler,
-        (uint32_t)TIM1_UP_TIM10_IRQHandler,
-        (uint32_t)TIM1_TRG_COM_TIM11_IRQHandler,
-        (uint32_t)TIM1_CC_IRQHandler,
-        (uint32_t)TIM2_IRQHandler,
-        (uint32_t)TIM3_IRQHandler,
-        (uint32_t)TIM4_IRQHandler,
-        (uint32_t)I2C1_EV_IRQHandler,
-        (uint32_t)I2C1_ER_IRQHandler,
-        (uint32_t)I2C2_EV_IRQHandler,
-        (uint32_t)I2C2_ER_IRQHandler,
-        (uint32_t)SPI1_IRQHandler,
-        (uint32_t)SPI2_IRQHandler,
-        (uint32_t)USART1_IRQHandler,
-        (uint32_t)USART2_IRQHandler,
-        (uint32_t)EXTI15_10_IRQHandler,
-        (uint32_t)RTC_Alarm_IRQHandler,
-        (uint32_t)OTG_FS_IRQHandler,
-        (uint32_t)DMA1_Stream7_IRQHandler,
-        (uint32_t)SDIO_IRQHandler,
-        (uint32_t)TIM5_IRQHandler,
-        (uint32_t)SPI3_IRQHandler,
-        (uint32_t)DMA2_Stream0_IRQHandler,
-        (uint32_t)DMA2_Stream1_IRQHandler,
-        (uint32_t)DMA2_Stream2_IRQHandler,
-        (uint32_t)DMA2_Stream3_IRQHandler,
-        (uint32_t)DMA2_Stream4_IRQHandler,
-        (uint32_t)OTG_FS_IRQHandler,
-        (uint32_t)DMA2_Stream5_IRQHandler,
-        (uint32_t)DMA2_Stream6_IRQHandler,
-        (uint32_t)DMA2_Stream7_IRQHandler,
-        (uint32_t)USART6_IRQHandler,
-        (uint32_t)I2C3_EV_IRQHandler,
-        (uint32_t)I2C3_ER_IRQHandler,
-        (uint32_t)FPU_IRQHandler,
-        (uint32_t)SPI4_IRQHandler,
-        (uint32_t)SPI5_IRQHandler
+        (uint32_t)&PendSV_Handler,
+        (uint32_t)&Systick_Handler,
+        (uint32_t)&WWDG_IRQHandler,
+        (uint32_t)&PVD_IRQHandler,
+        (uint32_t)&TAMP_STAMP_IRQHandler,
+        (uint32_t)&RTC_WKUP_IRQHandler,
+        (uint32_t)&FLASH_IRQHandler,
+        (uint32_t)&RCC_IRQHandler,
+        (uint32_t)&EXTI0_IRQHandler,
+        (uint32_t)&EXTI1_IRQHandler,
+        (uint32_t)&EXTI2_IRQHandler,
+        (uint32_t)&EXTI3_IRQHandler,
+        (uint32_t)&EXTI4_IRQHandler,
+        (uint32_t)&DMA1_Stream0_IRQHandler,
+        (uint32_t)&DMA1_Stream1_IRQHandler,
+        (uint32_t)&DMA1_Stream2_IRQHandler,
+        (uint32_t)&DMA1_Stream3_IRQHandler,
+        (uint32_t)&DMA1_Stream4_IRQHandler,
+        (uint32_t)&DMA1_Stream5_IRQHandler,
+        (uint32_t)&DMA1_Stream6_IRQHandler,
+        (uint32_t)&ADC_IRQHandler,
+        (uint32_t)&EXTI9_5_IRQHandler,
+        (uint32_t)&TIM1_BRK_TIM9_IRQHandler,
+        (uint32_t)&TIM1_UP_TIM10_IRQHandler,
+        (uint32_t)&TIM1_TRG_COM_TIM11_IRQHandler,
+        (uint32_t)&TIM1_CC_IRQHandler,
+        (uint32_t)&TIM2_IRQHandler,
+        (uint32_t)&TIM3_IRQHandler,
+        (uint32_t)&TIM4_IRQHandler,
+        (uint32_t)&I2C1_EV_IRQHandler,
+        (uint32_t)&I2C1_ER_IRQHandler,
+        (uint32_t)&I2C2_EV_IRQHandler,
+        (uint32_t)&I2C2_ER_IRQHandler,
+        (uint32_t)&SPI1_IRQHandler,
+        (uint32_t)&SPI2_IRQHandler,
+        (uint32_t)&USART1_IRQHandler,
+        (uint32_t)&USART2_IRQHandler,
+        (uint32_t)&EXTI15_10_IRQHandler,
+        (uint32_t)&RTC_Alarm_IRQHandler,
+        (uint32_t)&OTG_FS_IRQHandler,
+        (uint32_t)&DMA1_Stream7_IRQHandler,
+        (uint32_t)&SDIO_IRQHandler,
+        (uint32_t)&TIM5_IRQHandler,
+        (uint32_t)&SPI3_IRQHandler,
+        (uint32_t)&DMA2_Stream0_IRQHandler,
+        (uint32_t)&DMA2_Stream1_IRQHandler,
+        (uint32_t)&DMA2_Stream2_IRQHandler,
+        (uint32_t)&DMA2_Stream3_IRQHandler,
+        (uint32_t)&DMA2_Stream4_IRQHandler,
+        (uint32_t)&OTG_FS_IRQHandler,
+        (uint32_t)&DMA2_Stream5_IRQHandler,
+        (uint32_t)&DMA2_Stream6_IRQHandler,
+        (uint32_t)&DMA2_Stream7_IRQHandler,
+        (uint32_t)&USART6_IRQHandler,
+        (uint32_t)&I2C3_EV_IRQHandler,
+        (uint32_t)&I2C3_ER_IRQHandler,
+        (uint32_t)&FPU_IRQHandler,
+        (uint32_t)&SPI4_IRQHandler,
+        (uint32_t)&SPI5_IRQHandler
 
 };
 
 void Default_Handler(void)
 {
-    while (1);
+    while (1)
+        ;
 }
 
 void Reset_Handler(void)
 {
+    // 1. Copy .data section from FLASH to SRAM
+    uint32_t size = &end_of_data - &start_of_data;
+
+    uint8_t *p_src = (uint8_t *)&end_of_text;   // FLASH
+    uint8_t *p_dst = (uint8_t *)&start_of_data; // SRAM
+
+    for (uint32_t i = 0; i < size; i++)
+    {
+        *p_dst++ = *p_src++;
+    }
+
+    // 2. Initialize the .bss section to zero in SRAM
+    size = &end_of_bss - &start_of_bss;
+    p_dst = (uint8_t *)&start_of_bss;
+
+    for (uint32_t i = 0; i < size; i++)
+    {
+        *p_dst++ = 0;
+    }
+
+    // 3. Call main()
+    main();
 }
