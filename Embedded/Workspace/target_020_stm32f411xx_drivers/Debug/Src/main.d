30682c26d0529c4117f58b30f4a2e4c1ef0ed47a
Duc Trung <trung.lyduc18@gmail.com>
2023-10-08 01:14:04 +0700

    CHECKPOINT: working on header file of the driver

---
+++ Embedded/Workspace/target_020_stm32f411xx_drivers/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ D:/GIT/C-CPP/Embedded/Workspace/target_020_stm32f411xx_drivers/drivers/Inc/stm32f411xx.h
+D:/GIT/C-CPP/Embedded/Workspace/target_020_stm32f411xx_drivers/drivers/Inc/stm32f411xx.h:
---
+++ Embedded/Workspace/target_020_stm32f411xx_drivers/drivers/Inc/stm32f411xx.h
@@ -0,0 +1,22 @@
+
+#ifndef INC_STM32F411XX_H_
+#define INC_STM32F411XX_H_
+
+
+/*
+ * Base addresses of FLASH and SAM memories
+ * */
+#define FLASH_BASE_ADDR			0x08000000UL
+#define SRAM_BASE_ADDR			0x20000000UL
+#define ROM_BASE_ADDR			0x1FFF0000UL
+
+
+/*
+ * AHBx and APBx Bus Peripheral base addresses
+ */
+#define APB1_BASE_ADDR			0x40000000UL
+#define APB2_BASE_ADDR			0x40010000UL
+#define AHB1_BASE_ADDR			0x40020000UL
+#define AHB2_BASE_ADDR			0X50000000UL
+
+#endif /* INC_STM32F411XX_H_ */
