d8a9b9134fa978d7b7f44ff4f25d565e2413b191
Duc Trung <trung.lyduc18@gmail.com>
2023-10-08 15:04:47 +0700

    CHECKPOINT: addresses of peripherals

--- Embedded/Workspace/target_020_stm32f411xx_drivers/drivers/Inc/stm32f411xx.h
+++ Embedded/Workspace/target_020_stm32f411xx_drivers/drivers/Inc/stm32f411xx.h
@@ -1,22 +1,76 @@
-
 #ifndef INC_STM32F411XX_H_
 #define INC_STM32F411XX_H_
-
 
 /*
  * Base addresses of FLASH and SAM memories
  * */
-#define FLASH_BASE_ADDR			0x08000000UL
-#define SRAM_BASE_ADDR			0x20000000UL
-#define ROM_BASE_ADDR			0x1FFF0000UL
-
+#define FLASH_BASE_ADDR					(0x08000000UL)
+#define SRAM_BASE_ADDR					(0x20000000UL)
+#define ROM_BASE_ADDR					(0x1FFF0000UL)
 
 /*
- * AHBx and APBx Bus Peripheral base addresses
+ * Base addresses of AHBx and APBx bus peripherals
  */
-#define APB1_BASE_ADDR			0x40000000UL
-#define APB2_BASE_ADDR			0x40010000UL
-#define AHB1_BASE_ADDR			0x40020000UL
-#define AHB2_BASE_ADDR			0X50000000UL
+#define APB1_BASE_ADDR					(0x40000000UL)
+#define APB2_BASE_ADDR					(0x40010000UL)
+#define AHB1_BASE_ADDR					(0x40020000UL)
+#define AHB2_BASE_ADDR					(0X50000000UL)
+
+/*
+ *Base addresses of APB1 bus peripherals
+ */
+#define TIM2_BASE_ADDR					((APB1_BASE_ADDR)+(0x0000UL))
+#define TIM3_BASE_ADDR					((APB1_BASE_ADDR)+(0x0400UL))
+#define TIM4_BASE_ADDR					((APB1_BASE_ADDR)+(0x0800UL))
+#define TIM5_BASE_ADDR					((APB1_BASE_ADDR)+(0x0C00UL))
+#define RTC_BKP_REG_BASE_ADDR			((APB1_BASE_ADDR)+(0x2800UL))
+#define WWDG_BASE_ADDR					((APB1_BASE_ADDR)+(0x2C00UL))
+#define IWDG_BASE_ADDR					((APB1_BASE_ADDR)+(0x3000UL))
+#define I2S2EXT_BASE_ADDR				((APB1_BASE_ADDR)+(0x3400UL))
+#define SPI2_I2S2_BASE_ADDR				((APB1_BASE_ADDR)+(0x3800UL))
+#define SPI3_I2S3_BASE_ADDR				((APB1_BASE_ADDR)+(0x3C00UL))
+#define I2S3EXT_BASE_ADDR				((APB1_BASE_ADDR)+(0x4000UL))
+#define USART2_BASE_ADDR				((APB1_BASE_ADDR)+(0x4400UL))
+#define I2C1_BASE_ADDR					((APB1_BASE_ADDR)+(0x5400UL))
+#define I2C2_BASE_ADDR					((APB1_BASE_ADDR)+(0x5800UL))
+#define I2C3_BASE_ADDR					((APB1_BASE_ADDR)+(0x5C00UL))
+#define PWR_BASE_ADDR					((APB1_BASE_ADDR)+(0x7000UL))
+
+/*
+ * Base addresses of APB2 bus peripherals
+ */
+#define TIM1_BASE_ADDR					((APB2_BASE_ADDR)+(0x0000UL))
+#define USART1_BASE_ADDR				((APB2_BASE_ADDR)+(0x1000UL))
+#define USART6_BASE_ADDR				((APB2_BASE_ADDR)+(0x1400UL))
+#define ADC1_BASE_ADDR					((APB2_BASE_ADDR)+(0x2000UL))
+#define SDIO_BASE_ADDR					((APB2_BASE_ADDR)+(0x2C00UL))
+#define SPI1_I2S1_BASE_ADDR				((APB2_BASE_ADDR)+(0x3000UL))
+#define SPI4_I2S4_BASE_ADDR				((APB2_BASE_ADDR)+(0x3400UL))
+#define SYSCFG_BASE_ADDR				((APB2_BASE_ADDR)+(0x3800UL))
+#define EXTI_BASE_ADDR					((APB2_BASE_ADDR)+(0x3C00UL))
+#define TIM9_BASE_ADDR					((APB2_BASE_ADDR)+(0x4000UL))
+#define TIM10_BASE_ADDR					((APB2_BASE_ADDR)+(0x4400UL))
+#define TIM11_BASE_ADDR					((APB2_BASE_ADDR)+(0x4800UL))
+#define SPI5_I2S5_BASE_ADDR				((APB2_BASE_ADDR)+(0x5000UL))
+
+/*
+ *Base addresses of AHB1 bus peripherals
+ */
+#define GPIOA_BASE_ADDR					((AHB1_BASE_ADDR) + (0x0000UL))
+#define GPIOB_BASE_ADDR					((AHB1_BASE_ADDR) + (0x0400UL))
+#define GPIOC_BASE_ADDR					((AHB1_BASE_ADDR) + (0x0800UL))
+#define GPIOD_BASE_ADDR					((AHB1_BASE_ADDR) + (0x0C00UL))
+#define GPIOE_BASE_ADDR					((AHB1_BASE_ADDR) + (0x1000UL))
+#define GPIOH_BASE_ADDR					((AHB1_BASE_ADDR) + (0x1C00UL))
+#define CRC_BASE_ADDR					((AHB1_BASE_ADDR) + (0x3000UL))
+#define RCC_BASE_ADDR					((AHB1_BASE_ADDR) + (0x3800UL))
+#define FLASH_INTERFACE_REG_BASE_ADDR	((AHB1_BASE_ADDR) + (0x3C00UL))
+#define DMA1_BASE_ADDR					((AHB1_BASE_ADDR) + (0x6000UL))
+#define DMA2_BASE_ADDR					((AHB1_BASE_ADDR) + (0x6400UL))
+
+/*
+ *Base addresses of AHB1 bus peripherals
+ */
+#define USB_OTG_FS_BASE_ADDR			((AHB2_BASE_ADDR)+(0x0000UL))
 
 #endif /* INC_STM32F411XX_H_ */
