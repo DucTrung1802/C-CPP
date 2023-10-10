77d45fa0832f81329f2ff6e798fb2ef053296d0f
Duc Trung <trung.lyduc18@gmail.com>
2023-10-10 11:49:51 +0700

    CHECKPOINT: IRQ (interrupt request) Number of STM32F411xx MCU

--- Embedded/Workspace/target_020_stm32f411xx_drivers/drivers/Inc/stm32f411xx.h
+++ Embedded/Workspace/target_020_stm32f411xx_drivers/drivers/Inc/stm32f411xx.h
@@ -270,6 +270,69 @@ typedef struct
 #define GPIOH_REG_RESET()		do{(RCC->AHB1RSTR |= (1 << 7)); (RCC->AHB1RSTR &= ~(1 << 7));} while (0)
 
 /*
+ * IRQ (interrupt request) Number of STM32F411xx MCU
+ * NOTE: update these macros with valid values according to your MCU
+ * TODO: You may complete this list for other peripherals
+ */
+#define IRQ_WWDG					0
+#define IRQ_EXTI16_PVD				1
+#define IRQ_EXTI21_TAMP_STAMP		2
+#define IRQ_EXTI22_RTC_WKUP			3
+#define IRQ_FLASH					4
+#define IRQ_RCC						5
+#define IRQ_EXTI0					6
+#define IRQ_EXTI1					7
+#define IRQ_EXTI2					8
+#define IRQ_EXTI3					9
+#define IRQ_EXTI4					10
+#define IRQ_DMA1_STREAM0 			11
+#define IRQ_DMA1_STREAM1 			12
+#define IRQ_DMA1_STREAM2 			13
+#define IRQ_DMA1_STREAM3 			14
+#define IRQ_DMA1_STREAM4 			15
+#define IRQ_DMA1_STREAM5 			16
+#define IRQ_DMA1_STREAM6 			17
+#define IRQ_ADC						18
+#define IRQ_EXTI9_5					23
+#define IRQ_TIM1_BRK_TIM9			24
+#define IRQ_TIM1_UP_TIM10			25
+#define IRQ_TIM1_TRG_COM_TIM11		26
+#define IRQ_TIM1_CC					27
+#define IRQ_TIM2					28
+#define IRQ_TIM3					29
+#define IRQ_TIM4					30
+#define IRQ_I2C1_EV					31
+#define IRQ_I2C1_ER					32
+#define IRQ_I2C2_EV					33
+#define IRQ_I2C2_ER					34
+#define IRQ_SPI1					35
+#define IRQ_SPI2					36
+#define IRQ_USART1					37
+#define IRQ_USART2					38
+#define IRQ_EXTI15_10				40
+#define IRQ_EXTI17_RTC_Alarm		41
+#define IRQ_EXTI18_OTG_FS_WKUP		42
+#define IRQ_DMA1_STREAM7			47
+#define IRQ_SDIO					49
+#define IRQ_TIM5					50
+#define IRQ_SPI3					51
+#define IRQ_DMA2_STREAM0			56
+#define IRQ_DMA2_STREAM1			57
+#define IRQ_DMA2_STREAM2			58
+#define IRQ_DMA2_STREAM3			59
+#define IRQ_DMA2_STREAM4			60
+#define IRQ_OTG_FS					67
+#define IRQ_DMA2_STREAM5			68
+#define IRQ_DMA2_STREAM6			69
+#define IRQ_DMA2_STREAM7			70
+#define IRQ_USART6					71
+#define IRQ_I2C3_EV					72
+#define IRQ_I2C3_ER					73
+#define IRQ_FPU						81
+#define IRQ_SPI4					84
+#define IRQ_SPI5					85
+
+/*
  * Generic macros
  */
 #define ENABLE 			1
