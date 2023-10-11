b3208cc6a525448c46f9b6b3e5c559e2c00101bd
Duc Trung <trung.lyduc18@gmail.com>
2023-10-11 19:42:25 +0700

    CHECKPOINT: working on SPI peripheral 2

--- Embedded/Workspace/target_020_stm32f411xx_drivers/drivers/Inc/stm32f411xx_spi_driver.h
+++ Embedded/Workspace/target_020_stm32f411xx_drivers/drivers/Inc/stm32f411xx_spi_driver.h
@@ -19,10 +19,35 @@ typedef struct
 
 /*
  * Handle structure for SPIx peripheral
-*/
+ */
 typedef struct
 {
-	SPI_Reg
-};
+	SPI_RegDef_t *SPIx; /* This holds the base address of SPIx (x:0,1,2,3,4) peripheral */
+	SPI_Config_t SPIConfig;
+} SPI_Handler_t;
+
+/*
+ * Peripheral Clock Setup
+ */
+void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
+
+/*
+ * Init and DeInit
+ */
+void SPI_Init(SPI_Handler_t *pSPIHandler);
+void SPI_DeInit(SPI_RegDef_t *pSPIx);
+
+/*
+ * Data Send and Receive
+ */
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t len);
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx);
+
+/*
+ * IRQ Configuration and ISR handler
+ */
+void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
+void SPI_IRQHandler(SPI_Handler_t *pHandler);
 
 #endif /* INC_STM32F411XX_SPI_DRIVER_H_ */
---
+++ Embedded/Workspace/target_020_stm32f411xx_drivers/drivers/Src/stm32f411xx_spi_driver.c
@@ -0,0 +1,54 @@
+#include "stm32f411xx_spi_driver.h"
+
+/*
+ * Peripheral Clock Setup
+ */
+void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
+{
+
+}
+
+/*
+ * Init and DeInit
+ */
+void SPI_Init(SPI_Handler_t *pSPIHandler)
+{
+
+}
+
+void SPI_DeInit(SPI_RegDef_t *pSPIx)
+{
+
+}
+
+/*
+ * Data Send and Receive
+ */
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t len)
+{
+
+}
+
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx)
+{
+
+}
+
+/*
+ * IRQ Configuration and ISR handler
+ */
+void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi)
+{
+
+}
+
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority)
+{
+
+}
+
+void SPI_IRQHandler(SPI_Handler_t *pHandler)
+{
+
+}
+
