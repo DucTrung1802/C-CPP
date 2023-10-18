0a79508b08748fd8945f27050a5bb959defb7b18
Duc Trung <trung.lyduc18@gmail.com>
2023-10-18 15:31:38 +0700

    CHECKPOINT: target_022_FreeRTOS_helloworld

--- Embedded/Workspace/target_022_FreeRTOS_helloworld/Debug/Core/Src/main.d
+++ Embedded/Workspace/target_022_FreeRTOS_helloworld/Debug/Core/Src/main.d
@@ -25,7 +25,16 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/portable.h \
+ D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/task.h \
+ D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/list.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -54,3 +63,12 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/portable.h:
+D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/task.h:
+D:/GIT/C-CPP/Embedded/Workspace/target_022_FreeRTOS_helloworld/ThirdParty/FreeRTOS/include/list.h:
